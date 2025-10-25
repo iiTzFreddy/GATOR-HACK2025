5b73e89f70fa0c080b565cd4d385b0b7850840b4
iiTzFreddy <freddyrives001@gmail.com>
2025-10-24 23:59:14 -0400

    Implement Pillow

--- SportsPhotography.py
+++ SportsPhotography.py
@@ -1,9 +1,16 @@
+#Gemini API
 from google import genai
+#Pillow Libary
+from PIL import Image
+
 client = genai.Client(api_key="AIzaSyBVzUjYRJj5URDaFHxT2jpa7ZGm7IUiYFE")
-nums = [1,2,3,4,5,6]
+
+
+img_path = r"C:\Users\Typic\Desktop\GATORHACK2025\basketball_player.png"
+img = Image.open(img_path)
 response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents= f"Only give the average of {nums}"
+    contents= [f"Only tell my who is in this image",img]
 )
-
+print("Response:")
 print(response.text)
